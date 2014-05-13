f06a354aec3d6e84a869894cfeaf4ac8553544f7
Will Pearse <will.pearse@gmail.com>
2014-05-13 13:22:15 -0400

    This is a test

--- inst/unitTests/test_dispersion.R
+++ inst/unitTests/test_dispersion.R
@@ -1,6 +1,7 @@
 #This may need checking on other systems, because I'm using exact value checks with no consideration of precision...
 #Testing all the measures together is somewhat silly, because I can't set the set on anything so the randomisations match.
 #Setup
+#Here's a demo Matt
 require(testthat)
 data(phylocom)
 data <- comparative.comm(phylocom$phylo, phylocom$sample, warn=FALSE)
