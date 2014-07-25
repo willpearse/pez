63449d245bb4838e07d86224337f2c01121facff
stevencarlislewalker <scwalker@math.mcmaster.ca>
2014-07-25 14:49:07 -0400

    proposal for errors in shape (#1)

--- R/shape.R
+++ R/shape.R
@@ -1,7 +1,11 @@
-#TODO:
-# I'm not sure PD should be standardised by the sum of species, but rather the sum of individuals. In general, we need to have a better discussion about individual vs. species metrics and scaling
-# it's going to be hard for people to install ecoPD, so we need some sensible 'require'-foo to get everything in there
-# I don't like having to 'require' ape when using phylobase, but either I'm doing something really stupid with the dependencies or phylobase doesn't have a properly setup NAMESPACE. Grrrr.
+#TODO: I'm not sure PD should be standardised by the sum of species,
+# but rather the sum of individuals. In general, we need to have a
+# better discussion about individual vs. species metrics and scaling
+# it's going to be hard for people to install ecoPD, so we need some
+# sensible 'require'-foo to get everything in there I don't like
+# having to 'require' ape when using phylobase, but either I'm doing
+# something really stupid with the dependencies or phylobase doesn't
+# have a properly setup NAMESPACE. Grrrr.
 
 #' Calculate dissimilarity metrics across communities
 #' 
@@ -11,6 +15,7 @@
 #' @param metric specify (a) particular metric(s) to calculate (unifrac, pcd, phylosor), or the default 'all'
 #' @param pa If TRUE (default), all metrics are calculated across presence-absence matrices, and species abundances are ignored
 #' @param permute Number of permutations for metric (currently only for PCD)
+#' @param removeErrors If \code{FALSE}, metrics that failed to be computed are returned as error messages
 #' @details This calculates UniFrac, PCD (and its subcomponents), and the phylogenetic Sorenson's index, all defined as dissimilarity metrics in Pearse et al.
 #' @note This function uses XXX's version of the PCD function, not that included in picante
 #' @return cc.dissimilarity object (a named list with the output from each metric)
@@ -28,8 +33,16 @@
 #' @importFrom apTreeshape as.treeshape as.treeshape.phylo colless tipsubtree
 #' @importFrom ape gammaStat cophenetic.phylo drop.tip
 #' @export
-shape <- function(data, metric=c("all", "psv", "psr", "mpd", "pd", "colless", "gamma", "taxon", "eigen.sum", "cadotte.pd"), which.eigen=1)  #vecnums chooses the eigenvector to calculate sumvar in Diniz-Filho J.A.F., Cianciaruso M.V., Rangel T.F. & Bini L.M. (2011). Eigenvector estimation of phylogenetic and functional diversity. Functional Ecology, 25, 735-744.
+shape <- function(data,
+                  metric=c("all", "psv", "psr", "mpd", "pd",
+                      "colless", "gamma", "taxon", "eigen.sum", "cadotte.pd"),
+                  which.eigen=1, removeErrors = TRUE)
 {
+# vecnums chooses the eigenvector to calculate sumvar in Diniz-Filho
+# J.A.F., Cianciaruso M.V., Rangel T.F. & Bini
+# L.M. (2011). Eigenvector estimation of phylogenetic and functional
+# diversity. Functional Ecology, 25, 735-744.
+    
   #Assertions and argument handling
   if(!inherits(data, "comparative.comm"))  stop("'data' must be a comparative community ecology object")
   metric <- match.arg(metric)
@@ -43,46 +56,62 @@ shape <- function(data, metric=c("all", "psv", "psr", "mpd", "pd", "colless", "g
     data$vcv <- cophenetic(data$phy)
   output <- list(psv=NULL, psr=NULL, mpd=NULL, pd=NULL, pd.ivs=NULL, colless=NULL, gamma=NULL, taxon=NULL, eigen.sum=NULL, cadotte.pd=NULL)
   
-  #Caculate measures
+  #Calculate measures
   if(metric == "psv" | metric == "all")
-    output$psv <- psd(pa, data$phy)[,1]
+    output$psv <- try(psd(pa, data$phy)[,1], silent = TRUE)
   
   if(metric == "psr" | metric == "all")
-    output$psr <- psd(pa, data$phy)[,4]
+    output$psr <- try(psd(pa, data$phy)[,4], silent = TRUE)
   
   if(metric == "mpd" | metric == "all")
-    output$mpd <- mpd(pa, data$vcv)
+    output$mpd <- try(mpd(pa, data$vcv), silent = TRUE)
   
   if(metric == "pd" | metric == "all"){
-    output$pd <- pd(pa, data$phy)[,1]
-    output$pd.ivs <- resid(lm(output$pd ~ rowSums(pa)))
+      output$pd.ivs <- .trywith(output, {
+          pd <- pd(pa, data$phy)[,1]
+          resid(lm(pd ~ rowSums(pa)))
+      })
   }
   
   if(metric == "colless" | metric == "all"){
-    tree.shape <- as.treeshape(data$phy)
-    nams <- tree.shape$names
-    output$colless <- apply(pa, 1, .colless, tree.shape, nams)
+      output$colless <- .trywith(data, {
+          tree.shape <- as.treeshape(phy)
+          nams <- tree.shape$names
+          apply(pa, 1, .colless, tree.shape, nams)
+      })
   }
   
   if(metric == "gamma" | metric == "all"){
-    tree.shape <- as.treeshape(data$phy)
-    nams <- tree.shape$names
-    output$gamma <- apply(pa, 1, .gamma, data$phy, nams)
+      output$gamma <- .trywith(data, {
+          tree.shape <- as.treeshape(phy)
+          nams <- tree.shape$names
+          apply(pa, 1, .gamma, phy, nams)
+      })
   }
   
-  #Note - I've cut out some here because I think the simplification can happen in the summary output
+  #Note - I've cut out some here because I think the simplification
+  #can happen in the summary output
   if(metric == "taxon" | metric == "all")
-    output$taxon <- taxondive(pa, data$vcv)
+      output$taxon <- try(taxondive(pa, data$vcv), silent = TRUE)
   
   if(metric == "eigen.sum" | metric == "all"){
-      evc <- PVRdecomp(data$phy)@Eigen$vectors  
-      output$eigen.sum <- apply(pa, 1, 
-                                function(x, evc, vecnums) if(sum(x>0)) return(sum(apply(as.matrix(evc[x>0,vecnums]),2,var))) else return(NA), 
-                                evc,which.eigen)
+      output$eigen.sum <- .trywith(data, {
+          evc <- PVRdecomp(phy)@Eigen$vectors
+          apply(pa, 1, 
+                function(x, evc, vecnums) {
+                    if(sum(x>0)){
+                        return(sum(apply(as.matrix(evc[x>0,vecnums]),2,var)))
+                    } else {
+                        return(NA)
+                    }
+                }, evc, which.eigen)
+      })
   }
   
   if(metric == "cadotte.pd" | metric == "all")
-    output$cadotte.pd <- data.frame(Eed=.eed(data), Hed=.hed(data))
+    output$cadotte.pd <- try(data.frame(Eed=.eed(data), Hed=.hed(data)), silent = TRUE)
+
+  if(removeErrors) output <- lapply(output, .removeErrors)
   
   #Prepare output
   output$type <- "shape"
@@ -130,4 +159,12 @@ shape <- function(data, metric=c("all", "psv", "psr", "mpd", "pd", "colless", "g
     output <- .hed(data) / log(apply(data$comm, 1, function(x) sum(x != 0)))
     names(output) <- rownames(data)
     return(output)
+}
+
+
+.trywith <- function(data, expr) try(with(data, expr), silent = TRUE)
+
+.removeErrors <- function(object) {
+    if(inherits(object, "try-error")) return(NULL)
+    object
 }
--- misc/experimentingSW.R
+++ misc/experimentingSW.R
@@ -1,7 +1,8 @@
 library(pez)
+library(picante)
 library(ape)
-library(ade4)
-library(subscript)
+#library(ade4)
+#library(subscript)
 set.seed(1)
 
 n <- 8 # number of sites
@@ -15,14 +16,24 @@ traits <- data.frame(z1 = rnorm(m), z2 = rnorm(m))
 rownames(traits) <- LETTERS[1:m]
 newick <- "((A,(B,E)),(D,C));"
 tree <- chronos(compute.brlen(read.tree(text = newick)))
-comm <- matrix(rbinom(n*m, 1, 0.5), n, m)
+comm <- matrix(rbinom(n*m, 1, 0.8), n, m)
 dimnames(comm) <- list(rownames(env), rownames(traits))
 
 cc <- comparative.comm(tree, comm, traits, env)
+cc.shape <- shape(cc, removeErrors = FALSE)
+
+
+psd(comm, tree)
+psd(comm, vcv(tree))
+
+psv(comm, tree)
+psv(comm, vcv(tree))
+
+shape(cc, "psv")
+
+
 plot(cc)
 cc.barplot(cc, "z1")
-
-shape(cc)
 
 cc[1:2, 1:2]
 
