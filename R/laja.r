0df789f1329620a737e6bbf840921468b58085ff
mrhelmus <mrhelmus@gmail.com>
2014-08-05 17:02:19 -0400

    laja data set R and Rd files
    data(laja) still does not work with this addition of the .r and .rd
    files....

---
+++ R/laja.r
@@ -0,0 +1,11 @@
+#' @name laja
+#' @title Macroinvertebrate samples from the Rio Laja of Mexico
+#' @description This data set includes macroinvertebrate samples from the Rio Laja, a phylogenetic tree of the taxa and traits that include mean body length and fish feeding preference
+#' as in Helmus \emph{et al.} 2013.
+#' @docType data
+#' @usage laja
+#' @format \code{laja} contains a \code{\link{phylo}} object, a dataframe of sites-by-taxa, and a dataframe of traits
+#' @references Helmus M., Mercado-Silva N. & Vander Zanden M.J. (2013). Subsidies to predators, apparent competition and the phylogenetic structure of prey communities. Oecologia, 173, 997-1007.
+#' @author M.R. Helmus
+NULL
+
