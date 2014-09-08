4b850b8f9d46f5c382e8120d51dc3e87a52bb4c8
stevencarlislewalker <scwalker@math.mcmaster.ca>
2014-09-08 18:10:16 -0400

    pez-package help page

---
+++ R/pez-package.R
@@ -0,0 +1,16 @@
+#' Phylogenetics for the Environmental Sciences
+#'
+#' Eco-phylogenetic and community phylogenetic analyses.  Keeps
+#' community ecological and phylogenetic data matched up and
+#' comparable using 'comparative.comm' objects. Wrappers for common
+#' community phylogenetic indices ('shape', 'evenness', 'dispersion',
+#' and 'dissimilarity' metrics). Implementation of Cavender-Bares
+#' (2004) correlation of phylogenetic and ecological matrices
+#' ('fingerprint.regression'). Simulation of null assemblages, traits,
+#' and phylogenies ('scape', 'sim.meta.comm').
+#' 
+#' @docType package
+#' @name pez
+#' @aliases pez package-pez pez-package
+NULL
+
