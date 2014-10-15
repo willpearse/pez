980a9e5ff41d94c0276c7899be561392dc31ecb5
stevencarlislewalker <scwalker@math.mcmaster.ca>
2014-10-15 14:49:26 -0400

    within.comparative.comm

---
+++ R/within.R
@@ -0,0 +1,14 @@
+##' @export
+within.comparative.comm <- function(data, expr, ...){
+	
+	parent <- parent.frame()
+	e <- evalq(environment(), data, parent)
+	eval(substitute(expr), e)
+	
+	cc <- as.list(e)
+	class(cc) <- class(data)
+        return(cc)
+}
+
+
+## FIXME: is this dangerous somehow?
